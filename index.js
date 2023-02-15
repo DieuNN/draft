bcb417520a0d6f46e5851555ff43d4ee8c2882dc
dieu <nongngocdieu20122002@gmail.com>
2023-02-15 23:11:32 +0700

    m

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 const https = require('https');
 const fs = require('fs');
 const http = require('http')
+const express = require('express');
 
 const options = {
     key: fs.readFileSync('private.key'),
     cert: fs.readFileSync('certificate.crt')
 };
 
+const app = express()
+
 const hostname = '0.0.0.0';
 const port = 3001;
 const securePort = 3443;
 
-const httpsServer = https.createServer(options);
 
-const httpServer = http.createServer();
-
-
-
-httpServer.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', (req, res) => {
+    res.send("Home")
+})
+app.get('/hello', (req, res) => {
+    res.send('Hello, Your mom!');
 });
 
-httpsServer.listen(securePort, hostname, () => {
-    console.log(`Secure server running at https://${hostname}:${securePort}/`);
+// app.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+https.createServer(options, app).listen(securePort, hostname, (req, res) => {
+    console.log("Https server created")
 });
+http.createServer(app).listen(securePort, hostname, (req, res)=> {
+    console.log("HTTP server created")
+});
+
+// app.listen(options, securePort, hostname, () => {
+//     console.log(`Secure server running at https://${hostname}:${securePort}/`);
+// });
