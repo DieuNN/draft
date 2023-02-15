b46942731570cd0322ad33b60749323ab5fe7bee
dieu <nongngocdieu20122002@gmail.com>
2023-02-15 22:04:57 +0700

    m

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const https = require('https');
 const fs = require('fs');
+const http = require('http')
 
 const options = {
     key: fs.readFileSync('private.key'),
@@ -8,13 +9,26 @@ const options = {
 
 const hostname = '127.0.0.1';
 const port = 3001;
+const securePort = 3443;
 
-const server = https.createServer(options, (req, res) => {
+const httpsServer = https.createServer(options, (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello, World!\n');
 });
 
-server.listen(port, hostname, () => {
+const httpServer = https.createServer(options, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+
+
+httpServer.listen(port, hostname, () => {
     console.log(`Server running at https://${hostname}:${port}/`);
+});
+
+httpsServer.listen(securePort, hostname, () => {
+    console.log(`Secure server running at https://${hostname}:${port}/`);
 });
