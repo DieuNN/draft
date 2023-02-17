6a3bcb07e01c5630f35353429cc52a9f41ce8edc
dieu <nongngocdieu20122002@gmail.com>
2023-02-17 10:56:15 +0700

    Sửa vài lỗi nhỏ

--- index.js
+++ index.js
@@ -15,16 +15,29 @@ app.set("view engine", 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('empty');
 });
 
-const hostname = '0.0.0.0';
+// Gioi thieu cong ty
+app.get('/gioi-thieu-cong-ty', (req, res)=> {
+    res.render("gioi_thieu/gioi_thieu_cong_ty")
+})
+app.get('/lich-su-hinh-thanh', (req, res)=> {
+    res.render("gioi_thieu/lich_su_hinh_thanh")
+})
+app.get('/doi-tac', (req, res)=> {
+    res.render("gioi_thieu/doi_tac")
+})
+app.get('/doi-ngu-nhan-su', (req, res)=> {
+    res.render("gioi_thieu/doi_ngu_nhan_su")
+})
+
+
+
+
+const hostname = 'localhost';
 const port = 3000;
 const securePort = 3443;
-
-app.get('/hello', (req, res) => {
-    res.send('Hello, Your mom!');
-});
 
 app.get("/healthz", (req, res)=> {
     res.status(200)
