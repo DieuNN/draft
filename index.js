5d2cb5b061ebffb2c7e82ce3ef9a26b131617957
dieu <nongngocdieu20122002@gmail.com>
2023-02-19 17:54:13 +0700

    Thêm các menu sản phẩm

--- index.js
+++ index.js
@@ -19,65 +19,86 @@ app.get('/', (req, res) => {
 });
 
 // Gioi thieu cong ty
-app.get('/gioi-thieu-cong-ty', (req, res)=> {
+app.get('/gioi-thieu-cong-ty', (req, res) => {
     res.render("gioi_thieu/gioi_thieu_cong_ty")
 })
-app.get('/lich-su-hinh-thanh', (req, res)=> {
+app.get('/lich-su-hinh-thanh', (req, res) => {
     res.render("gioi_thieu/lich_su_hinh_thanh")
 })
-app.get('/doi-tac', (req, res)=> {
+app.get('/doi-tac', (req, res) => {
     res.render("gioi_thieu/doi_tac")
 })
-app.get('/doi-ngu-nhan-su', (req, res)=> {
+app.get('/doi-ngu-nhan-su', (req, res) => {
     res.render("gioi_thieu/doi_ngu_nhan_su")
 })
 
 // Du an
-app.get('/du-an', (req, res)=> {
+app.get('/du-an', (req, res) => {
     res.render("du_an/du_an")
 })
 
 // Hoat dong
-app.get('/hoat-dong', (req, res)=> {
+app.get('/hoat-dong', (req, res) => {
     res.render("hoat_dong/hoat_dong")
 })
 
+// Hỗ trợ
+app.get('/ho-tro', (req, res) => {
+    res.render("ho_tro/ho_tro")
+})
+
 // Lien he
-app.get('/lien-he', (req, res)=> {
+app.get('/lien-he', (req, res) => {
     res.render("lien_he/lien_he")
 })
 
 // San pham
-app.get('/san-pham/may-nen-khi-hitachi', (req, res)=> {
+app.get('/san-pham/may-nen-khi-hitachi', (req, res) => {
     res.render("san_pham/may_nen_khi_hitachi")
 })
-app.get('/san-pham/may-nen-khi-cao-ap-hertz', (req, res)=> {
+app.get('/san-pham/may-nen-khi-hitachi/may-nen-khi-truc-vit', (req, res) => {
+    res.render("san_pham/may_nen_khi_hitachi/may_nen_khi_truc_vit")
+})
+app.get('/san-pham/may-nen-khi-hitachi/may-nen-khi-piston-bebicon', (req, res) => {
+    res.render("san_pham/may_nen_khi_hitachi/may_nen_khi_piston_bebicon")
+})
+app.get('/san-pham/may-nen-khi-hitachi/may-nen-khi-truc-xoan', (req, res) => {
+    res.render("san_pham/may_nen_khi_hitachi/may_nen_khi_truc_xoan")
+})
+app.get('/san-pham/may-nen-khi-cao-ap-hertz', (req, res) => {
     res.render("san_pham/may_nen_khi_cao_ap_hertz")
 })
-app.get('/san-pham/may-tao-khi-nito', (req, res)=> {
+app.get('/san-pham/may-tao-khi-nito', (req, res) => {
     res.render("san_pham/may_tao_khi_nito")
 })
-app.get('/san-pham/may-say-khi', (req, res)=> {
+app.get('/san-pham/may-tao-khi-nito/may-tao-khi-nito-psi', (req, res) => {
+    res.render("san_pham/may_tao_khi_nito/may_tao_khi_nito_psi")
+})
+app.get('/san-pham/may-tao-khi-nito/may-tao-khi-nito-hitachi', (req, res) => {
+    res.render("san_pham/may_tao_khi_nito/may_tao_khi_nito_hitachi")
+})
+app.get('/san-pham/may-say-khi', (req, res) => {
     res.render("san_pham/may_say_khi")
 })
-app.get('/san-pham/may-bien-ap-abb-hitachi', (req, res)=> {
+app.get('/san-pham/may-say-khi/may-say-khi-psi', (req, res) => {
+    res.render("san_pham/may_say_khi/may_say_khi_psi")
+})
+app.get('/san-pham/may-say-khi/may-say-khi-orion', (req, res) => {
+    res.render("san_pham/may_say_khi/may_say_khi_orion")
+})
+app.get('/san-pham/may-bien-ap-abb-hitachi', (req, res) => {
     res.render("san_pham/may_bien_app_app_hitachi")
 })
-app.get('/san-pham/san-pham-khac', (req, res)=> {
+app.get('/san-pham/san-pham-khac', (req, res) => {
     res.render("san_pham/san_pham_khac")
 })
-
-
-
-
-
 
 
 const hostname = 'localhost';
 const port = 3000;
 const securePort = 3443;
 
-app.get("/healthz", (req, res)=> {
+app.get("/healthz", (req, res) => {
     res.status(200)
     res.send("OK")
 })
@@ -89,7 +110,7 @@ app.get("/healthz", (req, res)=> {
 https.createServer(options, app).listen(securePort, hostname, (req, res) => {
     console.log("Https server created")
 });
-http.createServer(app).listen(port, hostname, (req, res)=> {
+http.createServer(app).listen(port, hostname, (req, res) => {
     console.log("HTTP server created")
 });
 
